17e2e7944d9e66d716bb485b7d73936729923840
xioali <lipengbest@qq.com>
2018-03-26 01:22:17 +0800

    添加 RandomExtensions, StringExtensions 类

---
+++ NorthWind.Utils/RandomExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Utils
+{
+    public static class RandomExtensions
+    {
+        public static long NextLong(this Random random)
+        {
+            return (long)(random.NextDouble() * long.MaxValue);
+        }
+    }
+}
